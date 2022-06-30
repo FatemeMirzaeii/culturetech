00c82da1f24551fbbad527f2f787e292f4e50b87
fateme <mirzaei.f@parto.email>
2022-06-30 14:45:20 +0430

    adding static style file

---
+++ src/styles/static.js
@@ -0,0 +1,16 @@
+import {Dimensions} from 'react-native';
+
+export const WIDTH = Math.round(Dimensions.get('window').width);
+
+export const HEIGHT = Math.round(Dimensions.get('window').height);
+
+export const COLOR = {};
+
+export const FONT = {
+  regular: 'IRANSansMobile(FaNum)',
+  medium: 'IRANSansMobile(FaNum)_Medium',
+  bold: 'IRANSansMobile(FaNum)_Bold',
+  light: 'IRANSansMobile(FaNum)_Light',
+  ultra_light: 'IRANSansMobile(FaNum)_UltraLight',
+  black: 'IRANSansMobile(FaNum)_Black',
+};
