eeb4221ea0d5e078a5ad6473112a6e8756cc91e1
fateme <mirzaei.f@parto.email>
2022-07-17 18:49:19 +0430

    fixing employer home styles

--- src/screens/employer-home/index.js
+++ src/screens/employer-home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
-import {List, FAB} from 'react-native-paper';
+import {List, FAB, Divider} from 'react-native-paper';
 import * as Progress from 'react-native-progress';
 import Animated from 'react-native-reanimated';
 import BottomSheet from 'reanimated-bottom-sheet';
@@ -21,25 +21,28 @@ const EmployerHome = ({navigation}) => {
   );
   return (
     <SafeAreaView style={styles.container}>
-      <List.Item
-        title="فاطمه میرزایی"
-        description="مرکز داده"
-        onPress={() => navigation.navigate('Checklist')}
-        left={props => (
-          <Progress.Circle
-            progress={0.4}
-            thickness={6}
-            size={50}
-            color={COLORS.orange}
-            showsText
-            textStyle={{color: COLORS.orange}}
-            strokeCap="round"
-          />
-        )}
-        // style={{backgroundColor: 'red'}}
-        titleStyle={{fontFamily: FONTS.medium}}
-        descriptionStyle={{fontFamily: FONTS.regular}}
-      />
+      <List.Section style={styles.list}>
+        <List.Subheader style={styles.textStyle}>آنبوردینگ</List.Subheader>
+        <List.Item
+          title="فاطمه میرزایی"
+          description="مرکز داده"
+          onPress={() => navigation.navigate('Checklist')}
+          left={props => (
+            <Progress.Circle
+              progress={0.4}
+              thickness={6}
+              size={50}
+              color={COLORS.orange}
+              showsText
+              textStyle={{color: COLORS.orange}}
+              strokeCap="round"
+            />
+          )}
+          titleStyle={styles.textStyle}
+          descriptionStyle={styles.textStyle}
+        />
+      </List.Section>
+      <Divider />
       <BottomSheet
         ref={sheetRef}
         snapPoints={[700, 300, 0]}
--- src/screens/employer-home/style.js
+++ src/screens/employer-home/style.js
@@ -2,7 +2,11 @@ import {StyleSheet} from 'react-native';
 import {COLORS, FONTS} from '../../styles/static';
 
 export default StyleSheet.create({
-  container: {height: '100%'},
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+  },
   fab: {
     backgroundColor: COLORS.orange,
     position: 'absolute',
@@ -10,4 +14,9 @@ export default StyleSheet.create({
     right: 0,
     bottom: 0,
   },
+  textStyle: {
+    fontFamily: FONTS.regular,
+    textAlign: 'right',
+  },
+  list: {padding: 25},
 });
