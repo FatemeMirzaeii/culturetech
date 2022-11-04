7491a5437f0754e083bc21114143b0cfddda18dd
Fateme Mirzaei <m.fa2009@ymail.com>
2022-11-04 16:13:49 +0330

    checklist rewrite

--- src/screens/checklist/index.js
+++ src/screens/checklist/index.js
@@ -1,77 +1,87 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
-import {List, Divider, Checkbox} from 'react-native-paper';
-import {COLORS, FONTS} from '../../styles/static';
+import React, {useEffect} from 'react';
+import {SafeAreaView, View} from 'react-native';
+import {Divider, Checkbox} from 'react-native-paper';
+import {readData, saveData} from '../../functions';
 import styles from './style';
 
 const Checklist = ({navigation}) => {
-  const [value, setValue] = React.useState(true);
-  const [value2, setValue2] = React.useState(true);
-  const [checked, setChecked] = React.useState(false);
+  const [stepsStatus, setStepsStatus] = React.useState({});
+  const steps = [
+    {
+      id: 1,
+      text: 'تماس همراه فنی',
+    },
+    {
+      id: 2,
+      text: 'دریافت ایمیل فایل‌های آموزشی',
+    },
+    {
+      id: 3,
+      text: 'امضای قرارداد',
+    },
+    {
+      id: 4,
+      text: 'معارفه با اعضای تیم',
+    },
+    {
+      id: 5,
+      text: 'ناهار دوستانه با همراه فنی',
+    },
+    {
+      id: 6,
+      text: 'دریافت ولکام پکیج',
+    },
+    {
+      id: 7,
+      text: 'جلسه آنبوردینگ',
+    },
+    {
+      id: 8,
+      text: 'دریافت مستندات آشنایی با سازمان',
+    },
+    {
+      id: 9,
+      text: 'پکیج دوره آموزشی پذیرش سازمانی',
+    },
+    {
+      id: 10,
+      text: 'ارزیابی عملکرد ۳ ماهه',
+    },
+    {
+      id: 11,
+      text: 'تدوین اهداف عملکردی',
+    },
+    {
+      id: 12,
+      text: 'شرکت در نظرسنجی خودارزیابی',
+    },
+  ];
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const data = await readData('steps');
+    setStepsStatus(JSON.parse(data) ?? {});
+  };
   return (
     <SafeAreaView style={styles.container}>
-      {/* <List.Item
-        title="هماهنگی میز"
-        // description="مرکز داده"
-        // onPress={() => navigation.navigate('Checklist')}
-        left={props => <RadioButton.Item label="First item" value="first" />}
-        // style={{backgroundColor: 'red'}}
-        titleStyle={{fontFamily: FONTS.medium}}
-        descriptionStyle={{fontFamily: FONTS.regular}}
-      /> */}
-      <Checkbox.Item
-        label="هماهنگی اتاق یا میز"
-        labelStyle={styles.textStyle}
-        status={checked ? 'checked' : 'unchecked'}
-        onPress={() => {
-          setChecked(!checked);
-        }}
-      />
-      <Divider />
-      <Checkbox.Item
-        label="هماهنگی سیستم"
-        labelStyle={styles.textStyle}
-        status={checked ? 'checked' : 'unchecked'}
-        onPress={() => {
-          setChecked(!checked);
-        }}
-      />
-      <Divider />
-      <Checkbox.Item
-        label="دسترسی اکانت‌ها"
-        labelStyle={styles.textStyle}
-        status={checked ? 'checked' : 'unchecked'}
-        onPress={() => {
-          setChecked(!checked);
-        }}
-      />
-      <Divider />
-      <Checkbox.Item
-        label="ارسال دعوتنامه به کانال ها و گروه ها"
-        labelStyle={styles.textStyle}
-        status={checked ? 'checked' : 'unchecked'}
-        onPress={() => {
-          setChecked(!checked);
-        }}
-      />
-      <Divider />
-      <Checkbox.Item
-        label="ارسال بروشورها"
-        labelStyle={styles.textStyle}
-        status={checked ? 'checked' : 'unchecked'}
-        onPress={() => {
-          setChecked(!checked);
-        }}
-      />
-      <Divider />
-      <Checkbox.Item
-        label="ولکام پکیج"
-        labelStyle={styles.textStyle}
-        status={checked ? 'checked' : 'unchecked'}
-        onPress={() => {
-          setChecked(!checked);
-        }}
-      />
+      {steps.map(i => (
+        <View key={i.id}>
+          <Checkbox.Item
+            label={i.text}
+            labelStyle={styles.textStyle}
+            status={stepsStatus[i.id] ? 'checked' : 'unchecked'}
+            onPress={() => {
+              const ss = {...stepsStatus, [i.id]: !stepsStatus[i.id]};
+              setStepsStatus(ss);
+              saveData('steps', JSON.stringify(ss));
+            }}
+          />
+          <Divider />
+        </View>
+      ))}
     </SafeAreaView>
   );
 };
--- src/screens/checklist/style.js
+++ src/screens/checklist/style.js
@@ -4,8 +4,6 @@ import {COLORS, FONTS} from '../../styles/static';
 export default StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%',
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
     padding: 10,
   },
   textStyle: {
