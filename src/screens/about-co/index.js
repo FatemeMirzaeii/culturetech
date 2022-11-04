ed2ae1bbf8034d05a34003c7fe860e6fa13907c9
Fateme Mirzaei <m.fa2009@ymail.com>
2022-11-04 17:12:56 +0330

    rewrite about co and add pdf libs

--- src/screens/about-co/index.js
+++ src/screens/about-co/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {View} from 'react-native';
+import Pdf from 'react-native-pdf';
+import styles from './style';
 
 const AboutCo = () => {
+  const source = {uri: 'bundle-assets://dotin.pdf', cache: true};
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hi!</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Pdf
+        source={source}
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`Number of pages: ${numberOfPages}`);
+        }}
+        onPageChanged={(page, numberOfPages) => {
+          console.log(`Current page: ${page}`);
+        }}
+        onError={error => {
+          console.log(error);
+        }}
+        onPressLink={uri => {
+          console.log(`Link pressed: ${uri}`);
+        }}
+        style={styles.pdf}
+      />
+    </View>
   );
 };
 export default AboutCo;
---
+++ src/screens/about-co/style.js
@@ -0,0 +1,16 @@
+import {StyleSheet, Dimensions} from 'react-native';
+import {COLORS, FONTS} from '../../styles/static';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 25,
+  },
+  pdf: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
